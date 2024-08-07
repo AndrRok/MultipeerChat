46a4262042a218335fba26ec8c81b663d55470e3
AndrRok <rokhmanov2000@yandex.ru>
2024-08-07 16:24:19 +0400

    Initial Commit

---
+++ MultipeerChat/MultipeerChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipeerChatApp.swift
+//  MultipeerChat
+//
+//  Created by ARMBP on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MultipeerChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
