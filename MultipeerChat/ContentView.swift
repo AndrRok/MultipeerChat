f7056f9837ebd6d53a0b70f62ff0885cc9637c1e
AndrRok <rokhmanov2000@yandex.ru>
2024-08-07 16:54:24 +0400

    HW10

--- MultipeerChat/ContentView.swift
+++ MultipeerChat/ContentView.swift
@@ -6,14 +6,38 @@
 //
 
 import SwiftUI
+import MultipeerConnectivity
 
 struct ContentView: View {
+    @StateObject private var multipeerManager = MultipeerManager()
+    @State private var messageText = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Status: \(multipeerManager.connectionStatus)")
+                .padding()
+            
+            List(multipeerManager.receivedMessages, id: \.self) { message in
+                Text(message)
+            }
+            
+            HStack {
+                TextField("Enter message", text: $messageText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button(action: {
+                    multipeerManager.send(message: messageText)
+                    messageText = ""
+                }) {
+                    Text("Send")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(5)
+                }
+                .padding()
+            }
         }
         .padding()
     }
